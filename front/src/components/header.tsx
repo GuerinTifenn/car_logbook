ec70365f6c74c273df1280f69b51e64a28eba646
GuerinTifenn <8807@holbertonstudents.com>
2024-09-20 15:30:25 +0200

    correct path icons and color logo

--- front/src/components/header.tsx
+++ front/src/components/header.tsx
@@ -1,16 +1,18 @@
 import React, { FC } from "react";
 import Image from 'next/image'
+import logo from '../public/assets/logo.svg';
+import userIconWhite from '../public/assets/user_white.svg';
 
 const Header: FC = ({}) => {
 	return (
 		<header>
 			<nav className="flex justify-between m-5">
-			<Image src='../assets/logo.svg'
+			<Image src={logo}
 			width={48}
 			height={48}
 			alt="Autolog logo">
 			</Image>
-			<Image src="../assets/user_white.svg"
+			<Image src={userIconWhite}
 			width={48}
 			height={48}
 			alt="user not connected logo">
